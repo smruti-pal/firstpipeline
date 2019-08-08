9d799acaca125b257866c32c5e23438e05ab5959
smruti-pal <48717516+smruti-pal@users.noreply.github.com>
2019-08-08 14:54:00 +0530

    in master branch  testing

--- src/main/java/com/firstpipeline/App.java
+++ src/main/java/com/firstpipeline/App.java
@@ -23,4 +23,10 @@ public class App
     {
         System.out.println( "in master branch" );
     }
+    
+    
+     public static void main( String[] args )
+    {
+        System.out.println( "in master branch  testing" );
+    }
 }
