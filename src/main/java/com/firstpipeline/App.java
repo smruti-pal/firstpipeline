29e14d1f3e4b6ddc0f522eaee763675406462cc7
smruti-pal <48717516+smruti-pal@users.noreply.github.com>
2019-08-08 14:33:51 +0530

    Update App.java

--- src/main/java/com/firstpipeline/App.java
+++ src/main/java/com/firstpipeline/App.java
@@ -10,4 +10,9 @@ public class App
     {
         System.out.println( "Hello smruti ranjan" );
     }
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "in test branch" );
+    }
 }
