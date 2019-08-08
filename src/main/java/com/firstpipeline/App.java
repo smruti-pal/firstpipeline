099c45bab6b671650c9df1b2738823aeae85fe93
smruti-pal <48717516+smruti-pal@users.noreply.github.com>
2019-08-08 14:34:47 +0530

    in master branch

--- src/main/java/com/firstpipeline/App.java
+++ src/main/java/com/firstpipeline/App.java
@@ -17,4 +17,10 @@ public class App
     {
         System.out.println( "Hello smruti ranjan" );
     }
+    
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "in master branch" );
+    }
 }
